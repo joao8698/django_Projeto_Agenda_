23727cba803dbe979cab91e4713ed0cd44d245bd
Joao Luiz <simp4kda@gmail.com>
2025-07-11 16:58:59 -0300

    new readme.md

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from contact.models import contact
+
 # Register your models here.
+
+@admin.register(contact)
+class contactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone',
+    ordering = '-id',
+    # list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 5
+    list_max_show_all = 1000
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# id (primary key) - gerado automaticamente pelo Django
+# first_name (string), last_name (string), phone (string)
+# email (email), created_date (date), description (text)
+
+#depois
+# category (foreign key), show (boolean), owner (foreign key)
+# picture (imagem)
+
+class contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
